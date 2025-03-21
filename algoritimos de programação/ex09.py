0d13737d0a10338d8371ea3b078e2fffc1b18e47
anderson-erick <erick.santos@univille.br>
2025-03-20 22:05:05 -0300

    Add files via upload

---
+++ algoritimos de programação/ex09.py
@@ -0,0 +1,14 @@
+# Construa um programa que calcule a quantidade de latas de tinta
+# necessárias e o custo para pintar tanques cilíndricos de combustível, onde
+# são fornecidos a altura e o raio desse cilindro. Sabendo que:
+# a lata de tinta custa R$20,00
+# cada lata contém 5 litros
+# cada litro de tinta pinta 3 metros quadrados.
+# área do cilindro=2*3.14*raio2
+# 2*3.14*raio*altura e que raio e
+# altura são dados de entrada.
+altura = float(input('Informe a altura'))
+raio = float(input('Informe o raio'))
+area = (2*3.14*raio**2)+(2*3.14*raio*altura)
+lata = 20,00
+
